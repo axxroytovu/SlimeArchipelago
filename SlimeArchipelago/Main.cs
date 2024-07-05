db45b77625601ca54bb2eec129d3004130a161c4
Sam Zimmerman <samzimmerman@me.com>
2024-07-05 16:55:32 -0700

    Add project files.

---
+++ SlimeArchipelago/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SRML;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimeArchipelago
+{
+    public class Main : ModEntryPoint
+    {
+        public override void PreLoad(){
+            HarmonyInstance.PatchAll();
+        }
+
+        public override void Load()
+        {
+            base.Load();
+        }
+
+        public override void PostLoad()
+        {
+            base.PostLoad();
+        }
+    }
+}
